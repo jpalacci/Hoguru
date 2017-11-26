2c6b65f13e0ae9ca1ac76f070805825476579b0a
fsammart <frperez@itba.edu.ar>
2017-11-26 12:58:28 -0300

    HotelAdministrator

---
+++ src/model/HotelAdministrator.java
@@ -0,0 +1,10 @@
+package model;
+
+//HotelAdministrator is not a Person in this model, it only has two attributes , userName and password
+
+public class HotelAdministrator  {
+
+    private String userName;
+    private String password;
+
+}
